95cfad9b8b461230427a62243ad05e448daa8d6f
NC <nicolas.candeli@cegetel.net>
2021-09-01 12:02:48 +0200

    Ajout d'une confirmation lors d'un envoi réussi et refactoring de la fonction de vérification

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -13,8 +13,6 @@ const modalBtn = document.querySelectorAll(".modal-btn");
 const form = document.querySelector("form");
 const formDatas = document.querySelectorAll(".formData");
 const modalBtnClose = document.querySelector(".close");
-
-// Form Elements
 const inputFirst = document.querySelector("#first");
 const inputLast = document.querySelector("#last");
 const inputEmail = document.querySelector("#email");
@@ -22,12 +20,7 @@ const inputBirthdate = document.querySelector("#birthdate");
 const inputTournamentQuantity = document.querySelector("#quantity");
 const inputTermsOfUse = document.querySelector("#checkbox1");
 
-// Regex
-const regexText = /^[a-zA-Z]{2,30}$/;
-const regexEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-const regexBirthdate = /^((((19[0-9][0-9])|(2[0-9][0-9][0-9]))([-])(0[13578]|10|12)([-])(0[1-9]|[12][0-9]|3[01]))|(((19[0-9][0-9])|(2[0-9][0-9][0-9]))([-])(0[469]|11)([-])([0][1-9]|[12][0-9]|30))|(((19[0-9][0-9])|(2[0-9][0-9][0-9]))([-])(02)([-])(0[1-9]|1[0-9]|2[0-8]))|(([02468][048]00)([-])(02)([-])(29))|(([13579][26]00)([-])(02)([-])(29))|(([0-9][0-9][0][48])([-])(02)([-])(29))|(([0-9][0-9][2468][048])([-])(02)([-])(29))|(([0-9][0-9][13579][26])([-])(02)([-])(29)))$/;
-const regexTournamentQuantity = /^[1-9]{0,1}[0-9]$/;
-
+// MODAL
 // Launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
@@ -44,87 +37,108 @@ function closeModal() {
   modalbg.style.display = "none";
 }
 
+// FORM VALIDATION
 // Add "novalidate" attribute to form
 form.setAttribute("novalidate", "");
-
-// Verify radio checkbox 
-function checkLocation() {
-  const formTournamentLocations = formDatas[5];
-  formTournamentLocations.classList.add("formTournamentLocations");
-  const inputTournamentLocations = document.querySelectorAll(".formTournamentLocations .checkbox-input");
-  for (const input of inputTournamentLocations) {
-    if (input.checked) {
-      return true;
-    }
-  }
-}
 
 // Add new divs for error messages
 for (const formData of formDatas) {
   const errorDiv = document.createElement("div");
   formData.appendChild(errorDiv); 
-  errorDiv.classList.add("errorMessage");
+  errorDiv.classList.add("errorDiv");
   errorDiv.style.color = "#fe142f";
   errorDiv.style.fontSize = ".55em";
 }
 
+// Add class for input types (input: DOM Element, error: error message, div: error div, regex: regular expression)
+class InputType {
+  constructor(input, error, div, regex) {
+  this.input = input;
+  this.error = error;
+  this.div = div;
+  this.regex = regex;
+  }
+  // Method#1 : Verify input value with regex
+  verifyInputValue() {
+    if (!this.regex.test(this.input.value)) {
+      this.div.textContent = this.error;
+      formIsValid = false;
+    } else {
+      this.div.textContent = "";
+    }
+  }
+  // Method#2 : Verify if input is checked 
+  verifyInputCheck(array) {
+    let failure = 0;
+    for (const input of array) {
+      if (!input.checked) {
+        failure++;
+      } if (failure === array.length) { 
+        this.div.textContent = this.error;
+        formIsValid = false;
+      } else {
+        this.div.textContent = "";
+      }
+    }
+  }
+}    
+
+// Elements used in method#2
+const inputTournamentLocations = document.querySelectorAll("input[name='location']");
+const inputTermsOfUseArray = [inputTermsOfUse];
+
+// Regular expressions
+const regexFirstAndLast = /^[a-zA-Z]{2,30}$/;
+const regexLast = /^[a-zA-Z]{2,30}$/;
+const regexEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+const regexBirthdate = /^((((19[0-9][0-9])|(2[0-9][0-9][0-9]))([-])(0[13578]|10|12)([-])(0[1-9]|[12][0-9]|3[01]))|(((19[0-9][0-9])|(2[0-9][0-9][0-9]))([-])(0[469]|11)([-])([0][1-9]|[12][0-9]|30))|(((19[0-9][0-9])|(2[0-9][0-9][0-9]))([-])(02)([-])(0[1-9]|1[0-9]|2[0-8]))|(([02468][048]00)([-])(02)([-])(29))|(([13579][26]00)([-])(02)([-])(29))|(([0-9][0-9][0][48])([-])(02)([-])(29))|(([0-9][0-9][2468][048])([-])(02)([-])(29))|(([0-9][0-9][13579][26])([-])(02)([-])(29)))$/;
+const regexTournaments = /^[1-9]{0,1}[0-9]$/;
+
+// Error messages
+const errorMessageFirst = "Veuillez saisir entre 2 et 30 caractères pour le champ du prénom.";
+const errorMessageLast = "Veuillez saisir entre 2 et 30 caractères pour le champ du nom.";
+const errorMessageEmail = "Veuillez saisir un email valide.";
+const errorMessageBirthdate = "Vous devez saisir votre date de naissance.";
+const errorMessageTournaments = "Veuillez entrer le nombre de tournois auxquels vous avez participé (entre 0 et 99).";
+const errorMessageLocation = "Vous devez choisir une option.";
+const errorMessageTermsOfUse = "Vous devez vérifier que vous acceptez les termes et conditions."; 
+
+// NodeList to catch each error div
+const errorDivs = document.querySelectorAll(".errorDiv");
+
+// Add an instance for each input type
+const firstName = new InputType (inputFirst, errorMessageFirst, errorDivs[0], regexFirstAndLast);
+const lastName = new InputType (inputLast, errorMessageLast, errorDivs[1], regexFirstAndLast);
+const email = new InputType (inputEmail, errorMessageEmail, errorDivs[2], regexEmail);
+const birthdate = new InputType (inputBirthdate, errorMessageBirthdate, errorDivs[3], regexBirthdate);
+const tournaments = new InputType (inputTournamentQuantity, errorMessageTournaments, errorDivs[4], regexTournaments);
+const city = new InputType ("", errorMessageLocation, errorDivs[5]);
+const termsOfUse = new InputType ("", errorMessageTermsOfUse, errorDivs[6]);
+
 // Check if form is valid
 let formIsValid = true;
-function checkIfValid() {
+
+function checkIfFormIsValid() {
   formIsValid = true;
-  const errorDivs = document.querySelectorAll(".errorMessage");
-  if (!regexText.test(inputFirst.value)) {
-    errorDivs[0].textContent = "Veuillez saisir entre 2 et 30 caractères pour le champ du prénom.";
-    formIsValid = false;
-  } else {
-    errorDivs[0].textContent = "";
-  }
-  if (!regexText.test(inputLast.value)) {
-    errorDivs[1].textContent = "Veuillez saisir entre 2 et 30 caractères pour le champ du nom.";
-    formIsValid = false;
-  } else {
-    errorDivs[1].textContent = "";
-  }
-  if (!regexEmail.test(inputEmail.value)) {
-    errorDivs[2].textContent = "Veuillez saisir un email valide.";
-    formIsValid = false;
-  } else {
-    errorDivs[2].textContent = "";
-  }
-  if (!regexBirthdate.test(inputBirthdate.value)) {
-    errorDivs[3].textContent = "Vous devez saisir votre date de naissance.";
-    formIsValid = false;
-  } else {
-    errorDivs[3].textContent = "";
-  }
-  if (!regexTournamentQuantity.test(inputTournamentQuantity.value)) {
-    errorDivs[4].textContent = "Veuillez entrer le nombre de tournois auxquels vous avez participé (entre 0 et 99).";
-    formIsValid = false;
-  } else {
-    errorDivs[4].textContent = "";
-  }
-  if (!checkLocation()) {
-    errorDivs[5].textContent = "Vous devez choisir une option.";
-    formIsValid = false;
-  } else {
-    errorDivs[5].textContent = "";
-  }
-  if (!inputTermsOfUse.checked) {
-    errorDivs[6].textContent = "Vous devez vérifier que vous acceptez les termes et conditions.";
-    formIsValid = false;
-  } else {
-    errorDivs[6].textContent = "";
-  }
-  return formIsValid;
+  // Method#1
+  firstName.verifyInputValue();
+  lastName.verifyInputValue();
+  email.verifyInputValue();
+  birthdate.verifyInputValue();
+  tournaments.verifyInputValue();
+  // Method#2
+  city.verifyInputCheck(inputTournamentLocations);
+  termsOfUse.verifyInputCheck(inputTermsOfUseArray);
 }
 
 // Validate form on submit
 function validate() {
-  checkIfValid();
+  checkIfFormIsValid();
+  console.log(formIsValid)
   if (formIsValid === false) {
     return false;
   } else {
-    console.log("Form is VALID"); 
+    alert("Merci ! Votre réservation a bien été envoyée.");
     return true;
   }
 }
