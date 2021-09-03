0b28d3adb0d93902f3834ac8632c1886eb18da62
NC <nicolas.candeli@cegetel.net>
2021-09-03 18:05:18 +0200

    Transformation de la confirmation et ajout d'une vérification d'âge

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# Projet GameOn
-1. Forkez ce repo ;
-2. Il est conseillé d'utiliser VisualStudio Code et vous pouvez utiliser Docker, mais ce n'est pas obligatoire ;
-3. Il n'y a aucune dépendance ;
-4. Vous ne devez utiliser que du CSS personnalisé et du JavaScript pur, sans jQuery, Bootstrap ou autre librairie.
+# NicolasCandeli_4_07082021
+
+## Livrable P4
+
+* GitHub : https://github.com/NC81/NicolasCandeli_4_07082021.git
--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -8,36 +8,39 @@ function editNav() {
 }
 
 // DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
+const modalBackground = document.querySelector(".bground");
+const modalButton = document.querySelectorAll(".modal-btn");
 const form = document.querySelector("form");
 const formDatas = document.querySelectorAll(".formData");
-const modalBtnClose = document.querySelector(".close");
+const modalButtonClose = document.querySelector(".close");
 const inputFirst = document.querySelector("#first");
 const inputLast = document.querySelector("#last");
 const inputEmail = document.querySelector("#email");
 const inputBirthdate = document.querySelector("#birthdate");
 const inputTournamentQuantity = document.querySelector("#quantity");
 const inputTermsOfUse = document.querySelector("#checkbox1");
+const confirmBackground = document.querySelector(".bground-confirm");
+const confirmMessage = document.querySelector(".modal-confirm");
+const confirmButtonClose = document.querySelector(".close-confirm");
 
 // MODAL
 // Launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+modalButton.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // Launch modal form
 function launchModal() {
-  modalbg.style.display = "block";
+  modalBackground.style.display = "block";
 }
 
 // Close modal event
-modalBtnClose.addEventListener("click", closeModal);
+modalButtonClose.addEventListener("click", closeModal);
 
 // Close modal form
 function closeModal() {
-  modalbg.style.display = "none";
+  modalBackground.style.display = "none";
 }
 
-// FORM VALIDATION
+// VALIDATION
 // Add "novalidate" attribute to form
 form.setAttribute("novalidate", "");
 
@@ -46,8 +49,6 @@ for (const formData of formDatas) {
   const errorDiv = document.createElement("div");
   formData.appendChild(errorDiv); 
   errorDiv.classList.add("errorDiv");
-  errorDiv.style.color = "#fe142f";
-  errorDiv.style.fontSize = ".55em";
 }
 
 // Add class for input types (input: DOM Element, error: error message, div: error div, regex: regular expression)
@@ -59,49 +60,67 @@ class InputType {
   this.regex = regex;
   }
   // Method#1 : Verify input value with regex
-  verifyInputValue() {
+  verifyRegex() {
     if (!this.regex.test(this.input.value)) {
-      this.div.textContent = this.error;
+      this.div.classList.add("errorDivEnabled");     
+      this.div.textContent = this.error; 
       formIsValid = false;
     } else {
+      this.div.classList.remove("errorDivEnabled");
       this.div.textContent = "";
     }
   }
-  // Method#2 : Verify if input is checked 
-  verifyInputCheck(array) {
+  // Method#2 : Verify if date is valid
+  verifyDate() {
+    const birthdateValue = new Date(this.input.value);
+    const dateDifference = Date.now() - birthdateValue.getTime();
+    const ageInMilliseconds = new Date(dateDifference);
+    const ageInYears = Math.abs(ageInMilliseconds.getUTCFullYear() - 1970);
+    if (isNaN(birthdateValue.getTime()) || (ageInYears < 18)) {
+      this.div.classList.add("errorDivEnabled");
+      this.div.textContent = this.error;
+      formIsValid = false;
+    } else {
+      this.div.classList.remove("errorDivEnabled");
+      this.div.textContent = "";
+    }
+  }
+  // Method#3 : Verify if input is checked 
+  verifyChecked(array) {
     let failure = 0;
     for (const input of array) {
       if (!input.checked) {
         failure++;
       } if (failure === array.length) { 
+        this.div.classList.add("errorDivEnabled");
         this.div.textContent = this.error;
         formIsValid = false;
       } else {
+        this.div.classList.remove("errorDivEnabled");
         this.div.textContent = "";
       }
     }
   }
-}    
+}
 
-// Elements used in method#2
+// Elements used in method#3
 const inputTournamentLocations = document.querySelectorAll("input[name='location']");
 const inputTermsOfUseArray = [inputTermsOfUse];
 
 // Regular expressions
-const regexFirstAndLast = /^[a-zA-Z]{2,30}$/;
-const regexLast = /^[a-zA-Z]{2,30}$/;
+const regexFirstAndLast = /^[a-zA-Z]{2,50}$/;
+const regexLast = /^[a-zA-Z]{2,50}$/;
 const regexEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-const regexBirthdate = /^((((19[0-9][0-9])|(2[0-9][0-9][0-9]))([-])(0[13578]|10|12)([-])(0[1-9]|[12][0-9]|3[01]))|(((19[0-9][0-9])|(2[0-9][0-9][0-9]))([-])(0[469]|11)([-])([0][1-9]|[12][0-9]|30))|(((19[0-9][0-9])|(2[0-9][0-9][0-9]))([-])(02)([-])(0[1-9]|1[0-9]|2[0-8]))|(([02468][048]00)([-])(02)([-])(29))|(([13579][26]00)([-])(02)([-])(29))|(([0-9][0-9][0][48])([-])(02)([-])(29))|(([0-9][0-9][2468][048])([-])(02)([-])(29))|(([0-9][0-9][13579][26])([-])(02)([-])(29)))$/;
 const regexTournaments = /^[1-9]{0,1}[0-9]$/;
 
 // Error messages
-const errorMessageFirst = "Veuillez saisir entre 2 et 30 caractères pour le champ du prénom.";
-const errorMessageLast = "Veuillez saisir entre 2 et 30 caractères pour le champ du nom.";
+const errorMessageFirst = "Veuillez saisir entre 2 et 50 caractères pour le champ du prénom.";
+const errorMessageLast = "Veuillez saisir entre 2 et 50 caractères pour le champ du nom.";
 const errorMessageEmail = "Veuillez saisir un email valide.";
-const errorMessageBirthdate = "Vous devez saisir votre date de naissance.";
+const errorMessageBirthdate = "Vous devez saisir votre date de naissance (18 ans minimum).";
 const errorMessageTournaments = "Veuillez entrer le nombre de tournois auxquels vous avez participé (entre 0 et 99).";
 const errorMessageLocation = "Vous devez choisir une option.";
-const errorMessageTermsOfUse = "Vous devez vérifier que vous acceptez les termes et conditions."; 
+const errorMessageTermsOfUse = "Vous devez vérifier que vous acceptez les termes et conditions.";
 
 // NodeList to catch each error div
 const errorDivs = document.querySelectorAll(".errorDiv");
@@ -110,35 +129,43 @@ const errorDivs = document.querySelectorAll(".errorDiv");
 const firstName = new InputType (inputFirst, errorMessageFirst, errorDivs[0], regexFirstAndLast);
 const lastName = new InputType (inputLast, errorMessageLast, errorDivs[1], regexFirstAndLast);
 const email = new InputType (inputEmail, errorMessageEmail, errorDivs[2], regexEmail);
-const birthdate = new InputType (inputBirthdate, errorMessageBirthdate, errorDivs[3], regexBirthdate);
+const birthdate = new InputType (inputBirthdate, errorMessageBirthdate, errorDivs[3]);
 const tournaments = new InputType (inputTournamentQuantity, errorMessageTournaments, errorDivs[4], regexTournaments);
 const city = new InputType ("", errorMessageLocation, errorDivs[5]);
 const termsOfUse = new InputType ("", errorMessageTermsOfUse, errorDivs[6]);
 
 // Check if form is valid
-let formIsValid = true;
+let formIsValid;
 
 function checkIfFormIsValid() {
   formIsValid = true;
   // Method#1
-  firstName.verifyInputValue();
-  lastName.verifyInputValue();
-  email.verifyInputValue();
-  birthdate.verifyInputValue();
-  tournaments.verifyInputValue();
+  firstName.verifyRegex();
+  lastName.verifyRegex();
+  email.verifyRegex();
+  tournaments.verifyRegex();
   // Method#2
-  city.verifyInputCheck(inputTournamentLocations);
-  termsOfUse.verifyInputCheck(inputTermsOfUseArray);
+  birthdate.verifyDate();
+  // Method#3
+  city.verifyChecked(inputTournamentLocations);
+  termsOfUse.verifyChecked(inputTermsOfUseArray);
 }
 
 // Validate form on submit
-function validate() {
+function validate(e) {
+  e.preventDefault();
   checkIfFormIsValid();
-  console.log(formIsValid)
   if (formIsValid === false) {
     return false;
   } else {
-    alert("Merci ! Votre réservation a bien été envoyée.");
+    form.style.display = "none";
+    confirmMessage.style.display = "block";
     return true;
   }
 }
+
+// Validate event
+form.addEventListener("submit", validate);
+
+// Close confirmation box event
+confirmButtonClose.addEventListener("click", closeModal);
